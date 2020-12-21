5f0330b2ded8736aaa7107c9531ae33720a266b9
Wanqi Zhu <1213.ghs@gmail.com>
2020-12-21 17:47:12 -0600

    Cleaned up metadata/social links in config, removed twitter references

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,10 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer specializing in building (and occasionally designing) exceptional websites, applications, and everything in between.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    title: 'Wanqi Zhu',
+    description: 'Wanqi Zhu\'s personal website.',
+    siteUrl: 'https://wanqizhu.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
